fcfe83451f6fc185e7b63ceb3c333cad374d576f
Anastasiia A. <95031679+anastasiiaal@users.noreply.github.com>
2023-12-12 15:19:56 +0100

    cours 12.12 partie 1

--- ConsoleApp1/Program.cs
+++ ConsoleApp1/Program.cs
@@ -12,9 +12,6 @@ Console.WriteLine(robotMobile.SeDeplacer(thisRobotNewPoint));
 Console.WriteLine(robotMobile.Demarrer());
 Console.WriteLine(robotMobile.Nettoyer());
 
-Console.ReadLine();
-
-
 robot.Nom = "Eva";
 robotMobile.Avancer(10, 10);
 Console.WriteLine(robotMobile.CalculerDureeDeDeplacement(1, 10));
@@ -26,3 +23,63 @@ Console.WriteLine(robot.AfficherPosition());
 Console.WriteLine(robotMobile.AfficherPosition());
 Console.WriteLine(robot.Demarrer());
 
+Robot AfficherNomRobot (Robot robot, string nouveauNom)
+{
+    robot.Nom = nouveauNom;
+    return robot;
+}
+
+Robot robot2 = new Robot("Sally", 0, 0);
+var newName = AfficherNomRobot(robot2, "Not Sally Anymore");
+Console.WriteLine(newName.Nom);
+
+var x = 5;
+
+var result = add(ref x, 5);
+Console.WriteLine(result);
+
+int add(ref int x, int y)
+{
+    x = 10;
+    return x + y;
+}
+
+const int MAX_ROBOTS = 5;
+
+var robots = new RobotMobile[MAX_ROBOTS];
+
+RobotMobile robot01 = new RobotMobile("First Vally", 0, 0, 10, thisRobotPoint);
+RobotMobile robot02 = new RobotMobile("Second Vally", 1, 1, 5, thisRobotPoint);
+RobotMobile robot03 = new RobotMobile("Third Vally", 2, 2, 8, thisRobotPoint);
+
+robots[0] = robot01;
+robots[1] = robot02;
+robots[2] = robot03;
+
+RobotMobile robot04 = new RobotMobile("Fourth Vally", 2, 2, 1, thisRobotPoint);
+robots[2] = robot04;
+
+int calculateAverageSpeed(RobotMobile[] robots)
+{
+    int totalSpeed = 0;
+    int count = 0;
+
+    foreach (var robot in robots)
+    {
+        if (robot != null)
+        {
+            totalSpeed += robot.Vitesse;
+            count++;
+        }
+    }
+
+    if (count == 0) return 0;
+
+    return (int)totalSpeed / count;
+}
+
+int vitesseMoyenne = calculateAverageSpeed(robots);
+Console.WriteLine(vitesseMoyenne);
+
+
+Console.ReadLine();
